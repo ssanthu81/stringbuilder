081510428d524dbde45552ab5362437a28c5e2ca
ssanthu81 <101133033+ssanthu81@users.noreply.github.com>
2022-05-13 08:55:57 -0500

    Add files via upload

---
+++ extensionmethods/extensionmethods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+namespace stringbuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+
+            Stopwatch sw1 = new Stopwatch();
+            sw1.Start();
+
+
+
+            for(int i = 1; i <= 100000; i++)
+            {
+                s = s + i;
+           }
+
+            sw1.Stop();
+
+            Program s1 = new Program();
+            for (int i = 1; i <= 100000; i++)
+            {
+                s1.Append(i);
+            }
+            sw1.Stop();
+        }
+    }
+}
